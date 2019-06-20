4d58cc74c1c48247780f2455c8ccff63aabbf3b2
Jun Shimura <junshimura@gmail.com>
2019-06-20 22:35:08 +0900

    1st

---
+++ Assets/GameDirector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // UI部品を使うために必要!
+
+public class GameDirector : MonoBehaviour {
+
+    Transform roulette;
+    Text fortune;
+
+    void Start()
+    {
+        this.roulette = GameObject.Find("Roulette").transform;
+        this.fortune = GameObject.Find("Fortune").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float r = roulette.transform.rotation.eulerAngles.z;
+
+        //回転角度を表示
+        if (r >= 30.0f && r < 90.0f) {
+            fortune.text = "大吉";
+        }
+        else if (r >= 90.0f
+           && r < 150.0f) {
+            fortune.text = "大凶";
+        }
+        else if (r >= 150.0f
+           && r < 210.0f) {
+            fortune.text = "小吉";
+        }
+        else if (r >= 210.0f
+           && r < 270.0f) {
+            fortune.text = "末吉";
+        }
+        else if (r >= 270.0f
+           && r < 330.0f) {
+            fortune.text = "中吉";
+        }
+        else {
+            fortune.text = "凶";
+        }
+    }
+}
---
+++ Assets/RouletteController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour {
+
+    float rotSpeed = 0; //回転速度
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            this.rotSpeed = Random.Range(9.0f, 11.0f);
+        }
+
+        //回転速度分、ルーレットを回転させる
+        transform.Rotate(0, 0, rotSpeed);
+        // transform.localScale=new Vector3( rotSpeed,rotSpeed,1);
+        //ルーレットを減速させる
+        this.rotSpeed = this.rotSpeed * 0.96f;
+        
+        /*
+        //回転角度を表示
+        //Debug.Log(transform.rotation.eulerAngles.z);
+        if (transform.rotation.eulerAngles.z >= 30.0f
+           && transform.rotation.eulerAngles.z < 90.0f) {
+            Debug.Log("大吉");
+        }
+        else if (transform.rotation.eulerAngles.z >= 90.0f
+           && transform.rotation.eulerAngles.z < 150.0f) {
+            Debug.Log("大凶");
+        }
+        else if (transform.rotation.eulerAngles.z >= 150.0f
+           && transform.rotation.eulerAngles.z < 210.0f) {
+            Debug.Log("小吉");
+        }
+        else if (transform.rotation.eulerAngles.z >= 210.0f
+           && transform.rotation.eulerAngles.z < 270.0f) {
+            Debug.Log("末吉");
+        }
+        else if (transform.rotation.eulerAngles.z >= 270.0f
+           && transform.rotation.eulerAngles.z < 330.0f) {
+            Debug.Log("中吉");
+        }
+        else {
+            Debug.Log("凶");
+        }
+        */
+
+        // 配列を使う
+        //Debug.Log(transform.rotation.eulerAngles.z.ToString()
+        //    + "," + AngleToMessage(transform.rotation.eulerAngles.z));
+    }
+
+    /// <summary>
+    /// 角度からメッセージに変換
+    /// </summary>
+    /// <param name="angle">角度</param>
+    /// <returns>占いのメッセージ文字列</returns>
+    string AngleToMessage(float angle)
+    {
+        string[] message
+            = { "大吉", "大凶", "小吉", "末吉", "中吉", "凶" };
+        int index = (((Mathf.CeilToInt(angle) + 330)) % 360) / 60;
+        return message[index];
+    }
+}
